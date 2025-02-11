e0d9467243d08ff02b8d3ca1d5230692ec3c21bf
Lstark10 <lucas_mundo_louco@hotmail.com>
2025-02-10 22:49:09 -0300

    Formatting currency, creating an object for new expense and creating a function for new expense

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
 //Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
 
-  amount.value = value
+  //Transformar o valor em centavos
+  value = Number(value) / 100
 
+  // Atuzliza o valor do input
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  //Formatação o valor no padrão BRL(real)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
+}
+
+//Captura o evento de submit do usuario para obter os valores
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  //Cria o objeto com os detalhes da nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+  try {
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.")
+    console.log(error)
+  }
 }
